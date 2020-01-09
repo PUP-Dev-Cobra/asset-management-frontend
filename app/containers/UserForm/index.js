3e2bb000c83d9259c1f800d97167074b063cb5e1
Daniel Jimenez <jimenez.daniel.sampa@gmail.com>
2020-01-10 02:18:42 +0800

    user-form-page

--- app/containers/UserForm/index.js
+++ app/containers/UserForm/index.js
@@ -1,30 +1,11 @@
 import React from 'react'
-import { Grid, Segment, Header, Button, Select } from 'semantic-ui-react'
+import { Grid, Segment, Header, Button, Select, Form } from 'semantic-ui-react'
 
 const countryOptions = [
-  { key: 'af', value: 'af', text: 'Afghanistan' },
-  { key: 'ax', value: 'ax', text: 'Aland Islands' },
-  { key: 'al', value: 'al', text: 'Albania' },
-  { key: 'dz', value: 'dz', text: 'Algeria' },
-  { key: 'as', value: 'as', text: 'American Samoa' },
-  { key: 'ad', value: 'ad', text: 'Andorra' },
-  { key: 'ao', value: 'ao', text: 'Angola' },
-  { key: 'ai', value: 'ai', text: 'Anguilla' },
-  { key: 'ag', value: 'ag', text: 'Antigua' },
-  { key: 'ar', value: 'ar', text: 'Argentina' },
-  { key: 'am', value: 'am', text: 'Armenia' },
-  { key: 'aw', value: 'aw', text: 'Aruba' },
-  { key: 'au', value: 'au', text: 'Australia' },
-  { key: 'at', value: 'at', text: 'Austria' },
-  { key: 'az', value: 'az', text: 'Azerbaijan' },
-  { key: 'bs', value: 'bs', text: 'Bahamas' },
-  { key: 'bh', value: 'bh', text: 'Bahrain' },
-  { key: 'bd', value: 'bd', text: 'Bangladesh' },
-  { key: 'bb', value: 'bb', text: 'Barbados' },
-  { key: 'by', value: 'by', text: 'Belarus' },
-  { key: 'be', value: 'be', text: 'Belgium' },
-  { key: 'bz', value: 'bz', text: 'Belize' },
-  { key: 'bj', value: 'bj', text: 'Benin' },
+  { key: 'ad', value: 'ad', text: 'Admin' },
+  { key: 'tl', value: 'tl', text: 'Teller' },
+  { key: 'mm', value: 'mm', text: 'Member' },
+  { key: 'ap', value: 'ap', text: 'Approver' }
 ]
 
 export default () => {
@@ -35,11 +16,11 @@ export default () => {
           <Header as='h3'>User Form</Header>
           <Form>
             <Form.Field>
-              <label>Email Address</label>
-              <input placeholder='email address' type='email' />
+              <label>Name</label>
+              <input placeholder='Name' type='text'/>
             </Form.Field>
             <Form.Field>
-              <label>Email Address</label>
+              <label>Email Address</label>  
               <input placeholder='email address' type='email' />
             </Form.Field>
             <Form.Group width='equal'>
@@ -52,13 +33,24 @@ export default () => {
                 <input placeholder='password' type='password' />
               </Form.Field>
             </Form.Group>
+            <Form.Group width='equal'>
             <Form.Field>
               <label>type</label>
-              <Select placeholder='User Type' options={countryOptions} />
+              <Select placeholder='User Type' options={countryOptions} /> 
             </Form.Field>
+            <Form.Field>
+              <label>Member</label>
+              <Select placeholder='Member'/>
+            </Form.Field>
+            </Form.Group>
+            <Form.Group width='eqal'>
             <Form.Field>
               <Button primary>Submit</Button>
             </Form.Field>
+            <Form.Field>
+              <Button primary>Disable</Button>
+            </Form.Field>
+            </Form.Group>
           </Form>
         </Segment>
       </Grid.Column>
