a9feb29ee7524ae40c080372b2e130dea1ed0060
abarcenas <aldrich.barcenas@gmail.com>
2020-01-10 11:21:49 +0800

    Add DashboardTeller route

--- app/rootContainers/Dashboard/index.js
+++ app/rootContainers/Dashboard/index.js
@@ -1,28 +1,29 @@
-import React from 'react'
+import React, { lazy } from 'react'
 import { Switch } from 'react-router-dom'
+import { Menu, Button, Image, Dropdown } from 'semantic-ui-react'
 
 import RouteWithSubroutes from 'Components/RouteWithSubRoutes'
-import { Menu, Button , Image ,Dropdown} from 'semantic-ui-react'
+
 import logoImg from 'Assets/logo.jpg'
 
-
+const LoadTellerDashboard = lazy(() => import('Containers/DashboardTeller' /* webpackChunkName: "Container-Home" */))
 
 const DashboardRoot = ({ routes, match }) => {
   const { isExact } = match
   return (
-    <div> 
+    <div>
       <Menu size='large'>
-        <Menu.Item >
-          <Image src={logoImg} avatar/>
-        </Menu.Item>  
-        <Menu.Menu position="right">
+        <Menu.Item>
+          <Image src={logoImg} avatar />
+        </Menu.Item>
+        <Menu.Menu position='right'>
           <Menu.Item>
             <Button primary>
               Create
             </Button>
           </Menu.Item>
           <Menu.Item>
-            <Dropdown text='Profile' simple >
+            <Dropdown text='Profile' simple>
               <Dropdown.Menu>
                 <Dropdown.Item>
                   Account Profile
@@ -31,21 +32,24 @@ const DashboardRoot = ({ routes, match }) => {
                   Logout
                 </Dropdown.Item>
               </Dropdown.Menu>
-
             </Dropdown>
           </Menu.Item>
         </Menu.Menu>
       </Menu>
-        {
-          !isExact &&
-            <div>
-              <Switch>
-                {
-                  (routes.map((r, i) => <RouteWithSubroutes key={i} {...r} />))
-                }
-              </Switch>
-            </div>
-        }
+      {
+        isExact && match.path === '/dashboard' &&
+          <LoadTellerDashboard />
+      }
+      {
+        !isExact &&
+          <div>
+            <Switch>
+              {
+                (routes.map((r, i) => <RouteWithSubroutes key={i} {...r} />))
+              }
+            </Switch>
+          </div>
+      }
     </div>
   )
 }
