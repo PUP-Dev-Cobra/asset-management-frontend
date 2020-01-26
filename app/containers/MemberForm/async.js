ce2e7d89f412fd316191d987f2a2cc229405cd46
abarcenas <aldrich.barcenas@gmail.com>
2020-01-26 20:53:23 +0800

    commonize input fields

---
+++ app/containers/MemberForm/async.js
@@ -0,0 +1,23 @@
+import { API_URL } from 'App/constants'
+import qs from 'query-string'
+
+import { headers } from 'App/utils'
+
+export const option = async args => {
+  const query = qs.stringify({ option_name: args.option_name })
+
+  const res = await fetch(
+    `${API_URL}/options?${query}`,
+    {
+      method: 'GET',
+      headers
+    }
+  )
+
+  if (res.status !== 200) {
+    const body = await res.json()
+    throw new Error(body.error).message
+  }
+
+  return res.json()
+}
